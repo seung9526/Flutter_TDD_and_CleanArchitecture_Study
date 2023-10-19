20e3d7bceaf148f19aa07825c2b9f2782c35834d
seung9526 <95rus@naver.com>
2023-10-19 10:22:03 +0900

    📝Day_2(getUser_TDD)

--- test/src/authentication/domian/usecases/get_users_test.dart
+++ test/src/authentication/domian/usecases/get_users_test.dart
@@ -1,11 +1,13 @@
-
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_toy/src/authentication/domian/entities/user.dart';
 import 'package:flutter_toy/src/authentication/domian/repositories/authentication_repository.dart';
 import 'package:flutter_toy/src/authentication/domian/usecases/get_users.dart';
+import 'package:mocktail/mocktail.dart';
 
 import 'authentication_repository.mock.dart';
 
-void main(){
+void main() {
   late GetUsers usecase;
   late AuthenticationRepository repository;
 
@@ -14,7 +16,25 @@ void main(){
     usecase = GetUsers(repository);
   });
 
-  test('should call [AuthRepo.getUsers] and return [List<User>]',() async {
+  const tResponse = [User.empty()];
 
-  });
+  test(
+    'should call [AuthRepo.getUsers] and return [List<User>]',
+    () async {
+      // Arrange
+      when(
+        () => repository.getUsers(),
+      ).thenAnswer(
+        (_) async => const Right(tResponse),
+      );
+
+      // act
+      final result = await usecase();
+
+      expect(result, equals(const Right<dynamic, List<User>> (tResponse)));
+      verify(() => repository.getUsers()).called(1);
+      verifyNoMoreInteractions(repository);
+
+    },
+  );
 }
