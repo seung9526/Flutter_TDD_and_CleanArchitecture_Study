2b8e803b32a2e6526eff224e06ffcfba2bea6a67
seung9526 <95rus@naver.com>
2023-10-26 13:23:08 +0900

    📝Day_5(add_cubit)

---
+++ lib/src/authentication/domian/presentation/bloc/authentication_state.dart
@@ -0,0 +1,44 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_toy/src/authentication/domian/entities/user.dart';
+
+abstract class AuthenticationState extends Equatable {
+  const AuthenticationState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class AuthenticationInitial extends AuthenticationState {
+  const AuthenticationInitial();
+}
+
+class CreatingUser extends AuthenticationState {
+  const CreatingUser();
+}
+
+class GettingUsers extends AuthenticationState {
+  const GettingUsers();
+}
+
+class UserCreated extends AuthenticationState {
+  const UserCreated();
+}
+
+class UsersLoaded extends AuthenticationState {
+  const UsersLoaded(this. users);
+
+  final List<User> users;
+
+  @override
+  List<Object> get props => users.map((user) => user.id).toList();
+}
+
+class AuthenticationError extends AuthenticationState {
+  const AuthenticationError(this.message);
+
+  final String message;
+
+  @override
+  List<String> get props => [message];
+}
+
